14c4a152ebdb4423ad48881fce68647378e9d56b
Alex King <awk14@my.fsu.edu>
2016-11-18 18:08:28 +0000

    Lesson 7 final commit

---
+++ lesson7_Assignment/script.js
@@ -0,0 +1,19 @@
+/*
+    Project Name:  Recipe Display Application
+      
+    Author: Alex King
+      
+    Date:   11/18/16
+
+    Filename: script.js
+*/
+    
+    //displays the next element after the current target
+    function display(event) {
+        
+            $(event.currentTarget).next().fadeIn("slow");
+        
+    }//end of display
+    
+    //attach event listener to h3 elements to invoke display function when clicked
+    $("h3").click(display);
